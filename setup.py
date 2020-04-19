7d524a6dce567fa8ba4423217b1f1c332ccc1908
eunchan1001 <eunchan1001@gmail.com>
2020-04-19 12:10:32 +0900

    Update setup

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 from setuptools import find_packages, setup
 import setuptools
 
-try:  
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
 
 # Read in the README for the long description on PyPI
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-reqs = parse_requirements("requirements.txt", session=False)
-install_requires = [str(ir.req) for ir in reqs]
-
 setup(
       name='mphyspy',
-      version='0.1.9',
+      version='0.1.9.1',
       description='Python3 library for calculating college modern physics',
       long_description=readme,
       long_description_content_type="text/markdown",
@@ -24,7 +17,6 @@ setup(
       author_email='eunchan1001@gmail.com',
       license='MIT',
       install_requires=install_requires,
-      packages=find_packages(),
       classifiers=[
           "License :: OSI Approved :: MIT License",
           "Operating System :: OS Independent",
