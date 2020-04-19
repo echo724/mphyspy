c05e566525eb717bcc57cae1f01087c8db5d4da4
eunchan1001 <eunchan1001@gmail.com>
2020-04-19 11:32:03 +0900

    Update atom.py

--- mphyspy/qmechanic/atom.py
+++ mphyspy/qmechanic/atom.py
@@ -24,3 +24,24 @@ def spin_p(s):
 def energy(n):
     e=-13.6/n**2
     return e
+
+def tunneling():
+    E=float(input("Kinetic E: "))
+    U=float(input("Potential E: "))
+    m=float(input("Mass: "))
+    L=float(input("Width: "))
+
+    a=np.sqrt(2*m*(U-E))/ct.hbar
+    T=16*(E/U)*(1-E/U)*np.exp(-2*a*L)
+    return T
+
+def bremsstrahlung():
+    type=input("Z or f: ")
+    if type == 'f':
+        Z=int(input("Z: "))
+        result=(3*13.6/(4*ct.h))*(Z-1)**2
+    else:
+        f=float(input("f: "))
+        result=np.sqrt(f*4*ct.h/(3*13.6))+1
+    print("{0}: {1}".format(type,result))
+    return result
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Update Readme.md'
+git commit -m 'Update atom.py'
 git push
 
 rm -rf dist build mphyspy.egg-info
